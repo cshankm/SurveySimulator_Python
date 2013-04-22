465587b4a115c03f17488ad93d23473fea40c6dd
Cory Shankman <cshankm@gmail.com>
2013-04-22 12:31:11 -0700

    Added fuzz testing to test_kbo.py

--- test_kbo.py
+++ test_kbo.py
@@ -6,6 +6,15 @@ test = kbo(50, 0.1, 10, 30, 260, H = drawH(0.9, 13))
 
 print test.a, test.e, test.inc, test.capom, test.argperi, test.H
 
+#fuzz a by up to +- 10%
+test.a = fuzz(test.a, 10)
+
+print test.a
+
+#fuzz a by up to +- 30 AU
+test.a = fuzz(test.a, 30, type='abs')
+
+print test.a
 
 # Show error handling on e value
 test = kbo(50, 1.1, 10, 30, 260, H = drawH(0.9, 13))
